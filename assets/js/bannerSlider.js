28e53b3726d3e647fce9e9caf2666147898cdd8f
sergio-20 <nubesgladius20@gmail.com>
2019-06-28 15:50:43 -0700

    Simple Responsive E-Commerce Theme.

---
+++ assets/js/bannerSlider.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.banner-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000
+  });
+});
